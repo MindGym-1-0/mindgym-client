50fab5c90e9132d856311f552d5bd08ea828ed49
atertus <47945321+atertus@users.noreply.github.com>
2026-05-24 18:27:17 -0400

    Update page.tsx

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import AuthSidebar from '@/components/AuthSidebar';
+import AuthSidebar from 'components/AuthSidebar';
 
 export default function LogInPage() {
   const [formData, setFormData] = useState({
